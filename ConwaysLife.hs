036e621a22581ff681c684ceff364712118828bf
Derek VerLee <derekverlee@gmail.com>
2014-01-31 20:08:36 -0500

    Work it all out on a hunch..

--- ConwaysLife.hs
+++ ConwaysLife.hs
@@ -4,10 +4,14 @@ module ConwaysLife
         emptyUniverse,
         liveCells,
         isCellAlive,
-        setCellAlive)
+        setCellAlive,
+        nextGen)
        where
 
-data Position = Position Integer Integer deriving (Eq, Show)
+import Control.Applicative
+import  qualified Data.MultiSet as MS
+
+data Position = Position Integer Integer deriving (Eq, Show, Ord)
 data Universe = Universe [Position]
 
 emptyUniverse :: Universe
@@ -21,3 +25,20 @@ isCellAlive pos (Universe cells) = elem pos cells
 
 setCellAlive :: Position -> Universe -> Universe
 setCellAlive pos (Universe cells) = Universe (pos : cells)
+
+nextGen :: Universe -> Universe
+nextGen (Universe oldCells) =
+  Universe $ filterLive cellCounts
+  where
+    cellCounts = countAllTheThings $ map enumerateNeighbors oldCells
+
+countAllTheThings :: Ord a => [[a]] -> MS.MultiSet a
+countAllTheThings originals =
+  MS.unions $ map (MS.fromList) originals
+
+enumerateNeighbors :: Position -> [Position]
+enumerateNeighbors (Position x y) =
+  Position <$> [x-1 .. x+1] <*> [y-1 .. y+1]
+
+filterLive :: MS.MultiSet Position -> [Position]
+filterLive mSet = map fst $ filter ((==3).snd) $ MS.toOccurList mSet
--- tests-main.hs
+++ tests-main.hs
@@ -13,37 +13,71 @@ instance Test.QuickCheck.Arbitrary Position where
   arbitrary = liftM2 Position arbitrary arbitrary
 
 tests :: [Test.Framework.Test]
-tests = [
-  testGroup "Universe (Model tests)"
+tests =
   [
-    testCase "A empty universe is created with no live cells"
-    (assert $ null $ liveCells emptyUniverse),
-    testCase "A universe seeded with some cells has those cells as alive"
-    (liveCells (Universe [Position 0 1, Position 2 2 ])
-     @?= [Position 0 1, Position 2 2]),
-    testGroup "A position is defined by an ordered pair of integers"
+    testGroup "Conways Game Of Life"
     [
-      testCase "origin = origin" ((Position 0 0) @=? (Position 0 0)),
-      testCase "42,-7 = 42,-7"   ((Position 42 (-7)) @=? (Position 42 (-7))),
-      testCase "mirrored not equal"
-      (assert $ not $ (Position 42 (-7)) == (Position 42 7))
-    ],
-    testGroup "Each position in universe might have living cell"
-    [
-      testProperty "Newly created universe means all positions nonliving"
-      ( pAllCellsDead emptyUniverse ),
-      testCase "We can create a universe that only has a living cell at origin"
-      ( assert
-        $ isCellAlive (Position 0 0)
-        $ setCellAlive (Position 0 0) emptyUniverse ),
-      testCase "Regression, setting -2 1 as alive"
-      ( assert
-        $ isCellAlive (Position (-2) 1)
-        $ setCellAlive (Position (-2) 1) emptyUniverse ),
-      testProperty "If i set a cell as alive, that cell is indeed alive when I look at"
-      ( pCheckCellSetAlive )
+      testGroup "Universe (Model tests)"
+      [
+        testCase "A empty universe is created with no live cells"
+        ( assert $ null $ liveCells emptyUniverse),
+        testCase "A universe seeded with some cells has those cells as alive"
+        ( liveCells (Universe [Position 0 1, Position 2 2 ])
+          @?= [Position 0 1, Position 2 2]),
+        testGroup "A position is defined by an ordered pair of integers"
+        [
+          testCase "origin = origin" ((Position 0 0) @=? (Position 0 0)),
+          testCase "42,-7 = 42,-7"   ((Position 42 (-7)) @=? (Position 42 (-7))),
+          testCase "mirrored not equal"
+          ( assert $ not $ (Position 42 (-7)) == (Position 42 7))
+        ],
+        testGroup "Each position in universe might have living cell"
+        [
+          testProperty "Newly created universe means all positions nonliving"
+          ( pAllCellsDead emptyUniverse ),
+          testCase "We can create a universe that only has a living cell at origin"
+          ( assert
+            $ isCellAlive (Position 0 0)
+            $ setCellAlive (Position 0 0) emptyUniverse ),
+          testCase "Regression, setting -2 1 as alive"
+          ( assert
+            $ isCellAlive (Position (-2) 1)
+            $ setCellAlive (Position (-2) 1) emptyUniverse ),
+          testProperty "If i set a cell as alive, that cell is indeed alive when I look at"
+          ( pCheckCellSetAlive )
+        ]
+      ],
+      testGroup "Engine tests"
+      [
+        testCase "One cell at origin to no live cells"
+        ( assert $ null $ liveCells
+          ( nextGen $ setCellAlive (Position 0 0) emptyUniverse )),
+        testCase "Three seperated cells around origin to one at origin"
+        ( assert
+          (
+            (liveCells $
+             nextGen $
+             setCellAlive (Position 0 1) $
+             setCellAlive (Position (-1) (-1)) $
+             setCellAlive (Position 1 (-1)) emptyUniverse
+            )
+            ==
+            (liveCells $ setCellAlive (Position 0 0) emptyUniverse )
+          )
+        ),
+        testCase "Three vertically stacked yeilds three horizontally stacked"
+        ( assert
+          ((liveCells $ nextGen $
+            setCellAlive (Position 0 1) $
+            setCellAlive (Position 0 0) $
+            setCellAlive (Position 0 (-1)) emptyUniverse)
+           ==
+           [(Position (-1) 0), (Position 0 0), (Position 1 0)]))
+      ]
     ]
-  ]]
+  ]
+
+
 
 pAllCellsDead :: Universe -> Position -> Bool
 pAllCellsDead universe position =
@@ -54,7 +88,6 @@ pCheckCellSetAlive pos =
   let newUniverse = setCellAlive pos emptyUniverse
       in
    isCellAlive pos newUniverse
-
 
 main :: IO ()
 main = defaultMain tests
