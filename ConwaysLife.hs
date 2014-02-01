9c7823482f18951f401583785b344b075185057f
Derek VerLee <derekverlee@gmail.com>
2014-01-31 20:43:59 -0500

    Change representation from list to set

--- ConwaysLife.hs
+++ ConwaysLife.hs
@@ -9,28 +9,30 @@ module ConwaysLife
        where
 
 import Control.Applicative
-import  qualified Data.MultiSet as MS
+import qualified Data.MultiSet as MS
+import qualified Data.Set as Set
 
 data Position = Position Integer Integer deriving (Eq, Show, Ord)
-data Universe = Universe [Position]
+data Universe = Universe (Set.Set Position)
 
 emptyUniverse :: Universe
-emptyUniverse = Universe []
+emptyUniverse = Universe Set.empty
 
 liveCells :: Universe -> [Position]
-liveCells (Universe cells) = cells
+liveCells (Universe cells) = Set.toList cells
 
 isCellAlive :: Position -> Universe -> Bool
-isCellAlive pos (Universe cells) = elem pos cells
+isCellAlive pos (Universe cells) = Set.member pos cells
 
 setCellAlive :: Position -> Universe -> Universe
-setCellAlive pos (Universe cells) = Universe (pos : cells)
+setCellAlive pos (Universe cells) = Universe (Set.insert pos cells)
 
 nextGen :: Universe -> Universe
 nextGen (Universe oldCells) =
   Universe $ filterLive cellCounts
   where
-    cellCounts = countAllTheThings $ map enumerateNeighbors oldCells
+    cellCounts = countAllTheThings $ neighborsList
+    neighborsList = map enumerateNeighbors $ Set.toList oldCells
 
 countAllTheThings :: Ord a => [[a]] -> MS.MultiSet a
 countAllTheThings originals =
@@ -40,5 +42,6 @@ enumerateNeighbors :: Position -> [Position]
 enumerateNeighbors (Position x y) =
   Position <$> [x-1 .. x+1] <*> [y-1 .. y+1]
 
-filterLive :: MS.MultiSet Position -> [Position]
-filterLive mSet = map fst $ filter ((==3).snd) $ MS.toOccurList mSet
+filterLive :: MS.MultiSet Position -> Set.Set Position
+filterLive mSet = Set.fromList $
+                  map fst $ filter ((==3).snd) $ MS.toOccurList mSet
--- tests-main.hs
+++ tests-main.hs
@@ -7,6 +7,7 @@ import Test.QuickCheck
 import Test.Framework
 import Test.Framework.Providers.HUnit
 import Test.Framework.Providers.QuickCheck
+import Data.Set (fromList)
 import ConwaysLife
 
 instance Test.QuickCheck.Arbitrary Position where
@@ -22,7 +23,7 @@ tests =
         testCase "A empty universe is created with no live cells"
         ( assert $ null $ liveCells emptyUniverse),
         testCase "A universe seeded with some cells has those cells as alive"
-        ( liveCells (Universe [Position 0 1, Position 2 2 ])
+        ( liveCells (Universe $ fromList [Position 0 1, Position 2 2 ])
           @?= [Position 0 1, Position 2 2]),
         testGroup "A position is defined by an ordered pair of integers"
         [
